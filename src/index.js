e49c5f6f23f147c7b529889456f869f5fcda0697
LeNgocBao145 <himagito@gmail.com>
2025-06-28 16:57:27 +0700

    28/06/2025-16:57{Add query parameters}

--- src/index.js
+++ src/index.js
@@ -22,6 +22,15 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
+app.get('/news', (req, res) => {
+  res.render('news');
+});
+
+app.get('/search', (req, res) => {
+  console.log(req.query)
+  res.render('search');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
