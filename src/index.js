d0b27f66e11b11ea6a42d6461862cabf5df26eb0
LeNgocBao145 <himagito@gmail.com>
2025-06-28 10:06:27 +0700

    28/06/2025-10:05{Using static files method and SCSS}

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//HTTP logger
+app.use(morgan('combined'));
 
 //Template engine
 app.engine('hbs', handlebars.engine({
